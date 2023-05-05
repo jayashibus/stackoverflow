3ef2f6693cd91d5b04a01cc466e1e1cef016de15
jayashibus <jayashibu@gmail.com>
2023-05-05 10:06:19 +0530

    ListAll component Added

---
+++ src/Component/ListAll/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <div className="List-container">
+      <div className="Box-left">All Questions</div>
+      <div className="Box-right ">
+        <button> Ask Question </button>
+      </div>
+    </div>
+  );
+};
+
+export default List;
